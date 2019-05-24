3de9f108d36a7d70ed51e4666829587a6a898000
Benjamin Benčina <benjamin.bencina@gmail.com>
2019-05-24 12:34:03 +0200

    Nekaj komentarjev.

--- Vaje7/risi_ver.m
+++ Vaje7/risi_ver.m
@@ -1,4 +1,10 @@
 function [] = risi_ver(T1, T2, l, tol)
+% RISI_VER narise zvezno veriznico. Pri tem so:
+% T1 in T2 sta krajisci veriznice oblike T1(a,A) in
+% T2(b,B),
+% l je dolzina veriznice,
+% tol je toleranca, ki doloca natancnost priblizka
+% pri navadni iteraciji.
     a = T1(1); A = T1(2);
     b = T2(1); B = T2(2);
     z = isci_z(T1, T2, l, 1, tol);
--- Vaje8/risi_brah.m
+++ Vaje8/risi_brah.m
@@ -4,10 +4,12 @@ function [theta, k] = risi_brah(T1, T2)
     a = T1(1); A = T1(2);
     b = T2(1); B = T2(2);
     
+    %premaknemo toèke dol, da isci_theta naredi svoje
     bt = b-a; Bt = B-A;
     [theta, k] = isci_theta([bt Bt]);
     
     t = linspace(0, theta, 100);
+    % premaknemo brahistohrono nazaj gor
     x = @(t) 0.5 * k^2 * (t - sin(t)) + a;
     y = @(t) -0.5 * k^2 * (1 - cos(t)) + A;
     plot(x(t), y(t));
